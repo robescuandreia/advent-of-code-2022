7f1a69434c7c1578e4b40b383586165a5dd27e12
Andreia <robescu.andreia@gmail.com>
2022-12-01 20:36:51 +0100

    Added solution for the first problem

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,29 +1,48 @@
+import { i } from "vitest/dist/index-9f5bc072";
 import { getData, sum } from "../utils";
 
 const data = getData(__dirname);
 
 export const getElves = (input: string) => {
-  const elves = input
-    .trim()
-    .split("\n\n")
-    .map((elve) => elve.split("\n").map(Number));
+  const elves = input.replace(/(\r)/gm, "").trim().split("\n\n");
 
+  for (let i = 0; i < elves.length; i++) {
+    const elf = elves[i];
+    elves[i] = elf.split("\n").map((v) => Number(v));
+  }
   return elves;
 };
 
+// [
+//   [ 1000, 2000, 3000 ],
+//   [ 4000 ],
+//   [ 5000, 6000 ],
+//   [ 7000, 8000, 9000 ],
+//   [ 10000]
+// ]
+
 export const getGreatestCalorieSum = (input: string) => {
   const elves = getElves(input);
-  return Math.max(...elves.map(sum));
+  for (let i = 0; i < elves.length; i++) {
+    const elf = elves[i];
+    elves[i] = sum(elf);
+  }
+  return Math.max(...elves);
 };
 
 export const getGreatestCalorieSumOfTopThree = (input: string) => {
   const elves = getElves(input);
-  return sum(
-    elves
-      .map(sum)
-      .sort((a, b) => b - a)
-      .slice(0, 3)
-  );
+  for (let i = 0; i < elves.length; i++) {
+    const elf = elves[i]; //[ 1000, 2000, 3000 ]
+
+    let sum = 0;
+    for (let j = 0; j < elf.length; j++) {
+      sum = sum + elf[j];
+    }
+    elves[i] = sum;
+  }
+  elves.sort((a, b) => a - b);
+  return sum(elves.slice(-3));
 };
 
 console.log(getGreatestCalorieSum(data));
